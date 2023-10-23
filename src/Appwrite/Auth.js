25b41ffe6edd39190a50e881d1c263ac10b1fb6d
manmohan <manmohan33chandrakar@gmail.com>
2023-10-23 19:04:26 +0530

    create auth js

---
+++ src/Appwrite/Auth.js
@@ -0,0 +1,63 @@
+import conf from "../conf/conf";
+import { Client, Account, ID } from "appwrite";
+
+export class Authservice{
+    client = new Client()
+    account;
+
+    constructor(){
+        this.client.setEndpoint(conf.APPWRITE_URL)
+        .setProject(conf.APPWRITE_PROJECTID)
+        
+        // add client in account
+
+        this.account = new Account(this.client)
+    }
+
+    // promise for account
+
+    async createaccount({email,password,name}){
+        try{
+            const useraccount = await this.account.create(ID.unique(),email,password,name)
+            if(useraccount){
+                return this.login(email,password)
+            }else{
+                return useraccount;
+            }
+        }catch(e){
+            throw e
+        }
+    }
+
+    // login function
+
+    async login({email,password}){
+        try{
+            await this.account.createEmailSession(email,password)
+        }catch(e){
+            throw e;
+        }
+    }
+
+    async getcurrentuser(){
+        try {
+            return this.account.get()
+        } catch (e) {
+            console.log("error from getcurrenuser:- ",e)
+                       
+        }
+    }
+
+    async deletesession(){
+        try{
+            this.account.deleteSessions()
+        }catch(e){
+            console.log("error from deletesession:- ",e)
+            
+        }
+    }
+}   
+
+const authservice = new Authservice();
+
+export default authservice
